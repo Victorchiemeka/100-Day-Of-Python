60cf714c5c7cdab461c89b25fe133fddb940a887
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-08-06 06:41:02 +0100

    task done

--- Day5-loops/password.py
+++ Day5-loops/password.py
@@ -4,70 +4,33 @@ letters = [
     "a",
     "b",
     "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
+    # ... (the rest of the letters)
     "Y",
     "Z",
 ]
+
 numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
 
 print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
-myTotal = ""
+
+myTotal = []
 for characters in range(0, nr_letters):
     myTotal += random.choice(letters)
+
 for characters in range(0, nr_symbols):
     myTotal += random.choice(symbols)
 
 for characters in range(0, nr_numbers):
     myTotal += random.choice(numbers)
 
-print(myTotal)
+random.shuffle(myTotal)  # Shuffle the password characters in place
+password = ""
+for char in myTotal:
+    password += char
+
+print(password)
